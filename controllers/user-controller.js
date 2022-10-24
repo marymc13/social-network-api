86c1e2b916215316b66f3924b7bb83671b941106
Mary Paul <marymc135@hotmail.com>
2022-10-24 08:51:52 -0400

    user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,7 +13,7 @@ const userController = {
 
   // get one user by id
   getUserById({ params }, res) {
-    User.findOne({ _id: params.id })
+    User.findOne({ _id: params.userId })
       .then(dbUserData => {
         // If no user is found, send 404
         if (!dbUserData) {
@@ -34,6 +34,32 @@ createUser(req, res) {
       .then(dbUserData => res.json(dbUserData))
       .catch(err => res.status(400).json(err));
   },
+
+  //update user
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.userId }, body, { new: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  //delete user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.userId })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
 };
 
 module.exports = userController;
--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
         type: String,
         unique: true,
         required: true,
-        trinm: true
+        trim: true
     },
     email: {
         type: String,
